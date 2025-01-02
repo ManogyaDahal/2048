733db6b8eaeb8ff2ca25fd10538ef27f0338ca65
ManogyaDahal <bat16692@gmail.com>
2025-01-02 22:40:52 +0545

    Added game logic for downpress

--- 2048.c
+++ 2048.c
@@ -7,12 +7,51 @@ int gameMat[3][3];
 
 //functions 
 void init(); //Initialize the game 
-void drawBoard(); //drawing board
+void drawBoard(); //drawing board (And yes board is hardcoded)
+int gameWin(); //game Win condition 
+int randomGen(); //To generate 2 in random empty blocks
+
 
 int main(void)
 {
+    char ch; //character for inputing the values for the game
+
     init(); 
     drawBoard(); 
+    //game loop 
+    
+    while(true){
+        switch (ch) {
+            case 'w':
+                
+            break; 
+            case 'a':
+                
+            break; 
+            //logic for downpress
+            case 's':
+              for(int i=2; i<0; i--){
+                    for(int j=0; j<3; j++){
+                       if(gameMat[i][j] == gameMat[i-1][j] && gameMat[i][j] != 0){
+                            gameMat[i][j] *= 2; 
+                            if(i==2){
+                            gameMat[i-1][j] = gameMat[i-2][j]; 
+                            }else{
+                            gameMat[i-1][j] = 0;
+                            }
+                        }
+                    }
+                }
+            break; 
+            case 'd':
+                
+            break; 
+        }
+
+        if(gameWin()){
+            break; 
+        }
+    }
     return EXIT_SUCCESS;
 }
 
@@ -35,4 +74,15 @@ void drawBoard(){
         printf("------------\n"); 
         printf("| %d | %d | %d |\n",gameMat[2][0], gameMat[2][1], gameMat[2][2]);
         printf("-------------\n"); 
+}
+int gameWin(){
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(gameMat[i][j] == 2048){
+                return 1; 
+            }
+        }
+    }
+    return 0; 
 }
